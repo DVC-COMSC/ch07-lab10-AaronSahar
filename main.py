b048877cf784a558479e2613c694a2d60da1757a
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 21:36:07 -0700

    test

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-# ******************************
-# Make your Code
-# ******************************
+inputvalues = input()
+numbers = inputvalues.split()
+for i in range(len(numbers)):
+	numbers[i] = int(numbers[i]) 
+smallest = numbers[0]
+for i in range(len(numbers)):
+    smallest = numbers[i]
+    for j in range((len(numbers)-i)):
+        if smallest > numbers[j+i]:
+            smallest = numbers[j+i]
+            smallest_position = j+i
+    loan = numbers[i]
+    numbers.remove(smallest)
+    numbers.insert(smallest_position, loan)
+    numbers.remove(numbers[i])
+    numbers.insert(i, smallest)
+    print(numbers)
