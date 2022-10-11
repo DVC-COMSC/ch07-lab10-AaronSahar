f9d76d5f3e6f1119242e5babd1fa8df1eb269558
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 21:58:05 -0700

    test

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ for i in range(len(numbers)):
     for j in range((len(numbers)-i)):
         if smallest > numbers[j+i]:
             smallest = numbers[j+i]
-            smallest_position = j+i+1
+            smallest_position = j+i
     print(smallest)
     loan = numbers[i]
+    if i == 4:
+        smallest_position += 1
+    print("smallest position:", smallest_position)
     numbers.remove(smallest)
+    print("removing1:", numbers)
     numbers.insert(smallest_position, loan)
+    print("inserting1:", numbers)
     numbers.remove(numbers[i])
+    print("removing2:", numbers)
     numbers.insert(i, smallest)
+    print("inserting2:", numbers)
     print(numbers)
